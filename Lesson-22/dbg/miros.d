2d818144c453df398f62a7e623d374032fb74dcb
Aswinvt <aswinvt010@gmail.com>
2019-02-18 00:27:46 +0530

    Initial commit

---
+++ Lesson-22/dbg/miros.d
@@ -0,0 +1,3 @@
+.\dbg\miros.o: miros.c
+.\dbg\miros.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\dbg\miros.o: miros.h
---
+++ Lesson-23/dbg/miros.d
@@ -0,0 +1,4 @@
+.\dbg\miros.o: miros.c
+.\dbg\miros.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\dbg\miros.o: miros.h
+.\dbg\miros.o: ..\qpc\include\qassert.h
